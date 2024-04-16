170add7d1d4c8d64a175d114e47969852328f07c
Maddie Witman <mnwranger@gmail.com>
2024-04-16 14:47:25 -0400

    Set api URL for prod

--- src/webapp/src/routes/(app)/cards/+page.ts
+++ src/webapp/src/routes/(app)/cards/+page.ts
@@ -1,10 +1,12 @@
 import type { PageLoad } from "./$types";
+import PUBLIC_API_BASE_URL from '$env/static/public';
 
 export const load: PageLoad = async ({ fetch, params, url }) => {
 
     let offset = Number(url.searchParams.get("page"))
 
-    const res = await fetch(`http://localhost:8080/cards?page=${offset}`);
+
+    const res = await fetch(`${PUBLIC_API_BASE_URL}/cards?page=${offset}`);
     const items = await res.json()
 
     return {
