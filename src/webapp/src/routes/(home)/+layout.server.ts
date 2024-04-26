3a1b6cf696d15965875d884ec416398f6498d1a2
Maddie Witman <mnwranger@gmail.com>
2024-04-26 18:49:24 -0400

    Logging for why user isn't getting passed?

--- src/webapp/src/routes/(home)/+layout.server.ts
+++ src/webapp/src/routes/(home)/+layout.server.ts
@@ -3,6 +3,8 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	let user = locals.user
+	console.log(user)
+	console.log("HI CLOUDFLARE")
 	return {
 		user
 	}
