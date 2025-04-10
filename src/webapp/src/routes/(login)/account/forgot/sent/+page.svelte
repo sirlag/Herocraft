a1acc11719c245bc497dce04950a74b3ee54bf7f
Maddie Witman <mnwranger@gmail.com>
2025-04-10 01:23:58 -0400

    landing page for forgot email sent

---
+++ src/webapp/src/routes/(login)/account/forgot/sent/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import LoginWidget from '../../LoginWidget.svelte'
+	import { MailCheck } from 'lucide-svelte';
+</script>
+
+<LoginWidget
+	title="Password Reset Sent"
+	>
+	<div class="text-center">
+		<div class="flex justify-center m-2">
+			<MailCheck size={120} />
+		</div>
+		<div class="prose py-4">
+			A email has been sent containing a link to reset your password.
+			If you haven't received the password reset email, please check your spam folder. You can also retry after 10 minutes.
+		</div>
+		<div class="pt-4 pb-4">
+			<a href="/account/signin">Return to Sign In</a>
+		</div>
+	</div>
+
+</LoginWidget>
