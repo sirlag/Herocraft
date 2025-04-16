af657403c34385f1e3975b23765f0f830cc082e8
Maddie Witman <mnwranger@gmail.com>
2025-04-16 13:48:41 -0400

    log even more info try to figure out what's going on.

--- src/webapp/src/routes/(login)/account/forgot/+page.server.ts
+++ src/webapp/src/routes/(login)/account/forgot/+page.server.ts
@@ -6,10 +6,10 @@ import { forgotSchema } from './ForgotSchema.ts';
 import { fail, redirect } from '@sveltejs/kit';
 import { AccountURLs } from '$lib/routes.ts';
 
-export const load: PageServerLoad = async ()=> {
+export const load: PageServerLoad = async () => {
 	return {
 		form: await superValidate(zod(forgotSchema))
-	}
+	};
 };
 
 export const actions: Actions = {
@@ -22,23 +22,35 @@ export const actions: Actions = {
 		}
 
 		let data = form.data;
+		let body = JSON.stringify({ ...data })
+
+		console.log(`Attempting to send ${body} to ${AccountURLs.forgotPassword}`)
 
 		let forgotResponse = await fetch(new URL(AccountURLs.forgotPassword), {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify({...data})
+			body: body
 		});
+
+		// 		let loginResponse = await fetch(new URL(AccountURLs.signin), {
+		// 			method: 'POST',
+		// 			headers: {
+		// 				'Content-Type': 'application/json'
+		// 			},
+		// 			// credentials: 'include',
+		// 			body: JSON.stringify({ ...data })
+		// 		});
 
 
 		if (!forgotResponse.ok) {
-			console.error("Unable to send password Reset", forgotResponse);
+			console.log('Unable to send password Reset', forgotResponse);
 			return fail(500, {
-				form
-			})
+				form, forgotResponse
+			});
 		}
 
-		redirect(303, "/account/forgot/sent")
+		redirect(303, '/account/forgot/sent');
 	}
-}
+};
