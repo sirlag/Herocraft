a687ac6602f8863046c0c4523496ae2816d26ec1
Maddie Witman <mnwranger@gmail.com>
2025-05-09 17:02:21 -0400

    Fix log

--- src/main/kotlin/app/herocraft/plugins/Security.kt
+++ src/main/kotlin/app/herocraft/plugins/Security.kt
@@ -1,5 +1,6 @@
 package app.herocraft.plugins
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
@@ -9,10 +10,13 @@ import kotlinx.serialization.Serializable
 data class UserSession(val id: String, val email: String, val verified: Boolean)
 
 fun Application.configureSecurity() {
+
+    val logger = KotlinLogging.logger {}
+
     authentication {
         session<UserSession>("auth-session") {
             validate { session ->
-                log.debug("Validation user session: id = ${session.id}, email = ${session.email}, verified = ${session.verified}")
+                logger.debug { "Validation user session: id = ${session.id}, email = ${session.email}, verified = ${session.verified}" }
                 session
             }
             challenge {
